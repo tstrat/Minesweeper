f3b7da89c0711225b838702227a59695dc29ec4a
tstrat <tstrat@email.arizona.edu>
2017-03-15 11:06:18 -0700

    Added test directory

--- src/MineGrid.java
+++ src/MineGrid.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by travis on 3/13/17.
  */
@@ -16,6 +18,21 @@ public class MineGrid {
      */
     public MineGrid(int[][] testBoard) {
         // copy board directly (dont use variable, but copy of it)
-        board = testBoard.clone();
+        board = new int[testBoard.length][];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = Arrays.copyOf(testBoard[i], testBoard[i].length);
+        }
+    }
+
+    public int[][] getBoard() {
+        if (board == null) {
+            return board;
+        }
+        int[][] copyOfBoard;
+        copyOfBoard = new int[board.length][];
+        for (int i = 0; i < board.length; i++) {
+            copyOfBoard[i] = Arrays.copyOf(board[i], board[i].length);
+        }
+        return copyOfBoard;
     }
 }
